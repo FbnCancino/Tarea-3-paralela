f690d9576e23d476f3f1a8ad29af3cdef81efe0b
fabian <fabian.ecr92@gmail.com>
2018-09-10 23:05:40 -0300

    arreglo del string con funcion substr

--- tarea3.cpp
+++ tarea3.cpp
@@ -1,97 +1,84 @@
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
-#include <stdlib.h>
 #include <cstdlib>
 #include <string>
-#include <string.h>
 #include <vector> 
 
 using namespace std;
 
 bool verificar(string n){
-	if(n.length()>1){
-		return false;
-	}
-	else{
-		if(n[0]>47 && n[0]<58 || n[0]==91 || n[0]==93 || n[0]==59){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
+    if(n.length()>1){
+        return false;
+    }
+    else{
+        if(n[0]>47 && n[0]<58 || n[0]==91 || n[0]==93 || n[0]==59){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
 
+void mostrar(int matriz[][9]){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cout<<matriz[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 
-int Convierte (string valor){   
-	int n = atoi(valor.c_str()); 
+int convierte(string aux){   
+    int n = atoi(aux.c_str()); 
     return n; 
 }
 
-void faltantes(int matriz[][3], int n[], int a){
-    int aux =1;
-    for(int i=0; i<3;i++){
-        for(int j=0;j<3;j++){
-            if(matriz[i][j]==0){
-                matriz[i][j]=aux;                            
-                n[a++]=aux;
-                i=0;
-                j=0;
-            }         
-            aux++;           
-        }
-    }
-}
-
-void mostrar(int matriz[][3]){
-	for(int i=0;i<3;i++){
-		for(int j=0;j<3;j++){
-			cout<<matriz[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-}
-
-int main(int argc, string* argv[]){
+int main(int argc, char* argv[]){
 
      //variables necesarias
-    int  matriz[3][3];
+    int matriz[9][9];
     int ct=0 ,ct_usa=0;
-    int n[3], n_usa[9],  
-    string soap = argv[1];
-    string rest;  
-    for(int i=0; i<3; i++){
-		for(int j=0; j<3; j++){
-			matriz[i][j]=0;
-		}
-    }
-	//Para ingresar datos, se ingresa con el siguiente formato: ./ejecutable "[1;2;3]" "[4;5;6]" "[7;8;9]"
+    int posI, posJ, num;
+    string argu;
 
-	if (argc >2){
-		if(verificar(argv[1])== true){ // comprobamos que el argumento sean validos            
-            // recorremos para guardar el valor en la posicion indicada
-            for(int i=0; i< soap.length(); i++){ 
-                json= soap[i];
-                if(verificar(json)== true){
-                    n[ct]= convierte (json) - 48;
-                    ct++;
+// llenado de matriz con 0
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            matriz[i][j]=0;
+        }
+    }
+
+
+    //Para ingresar datos, se ingresa con el siguiente formato: ./ejecutable "[1;2;3]" "[4;5;6]" "[7;8;9]"
+for(int i=1;i<argc;i++){
+        argu=argv[i];
+        cout << endl;
+        if (argu.substr(0,1)=="[" && argu.substr(6,1)=="]"){
+            if(argu.substr(2,1)==";" && argu.substr(4,1)==";"){
+                if(verificar(argu.substr(1,1)) && verificar(argu.substr(3,1)) && verificar(argu.substr(5,1))){
+                    posI= convierte(argu.substr(1,1));
+                    posJ= convierte(argu.substr(3,1));
+                    num= convierte(argu.substr(5,1));
+                    while(argu!=""){
+                        if (posI >= 0 && posI < 9 && posJ>= 0 && posJ<9 && num >=1 && num <=9){
+                            if (matriz [posI][posJ]==0){
+                                matriz [posI][posJ]=num;
+                                ct ++;
+                            }
+                            else {
+                                cout << "Parametro ya ingresado previamente" << endl;
+                            }
+                        }
+                        argu=argu.substr(7);
+                    }
                 }
-                if(ct==3) {
-                    matriz[n[0]][n[1]]=n[2]; // se agrega el valor a la matriz
-                    ct=0;
-                    n_usa[ct_usa]=n[2]; // guardamos valor para no volver a utilizarlo
-                    ct_usa++;
-                }
+                else{cout << "Datos mal ingresados";}
             }
-            
-            faltantes (matriz,n_usa,ct_usa);
-            mostrar(matriz); 
+            else{cout << "Datos mal ingresados";}       
         }
-        else
-        {
-            cout<<"Datos mal ingresados"<<endl;
-        }
-	}
+        else{cout << "Datos mal ingresados";}   
+    }
+    mostrar(matriz); 
     return 0;
 }
